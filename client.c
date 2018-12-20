ad9d397935884db920b9d6724cec1d02f8371beb
Nandar Soe <38772142+snandar@users.noreply.github.com>
2018-12-20 04:50:03 -0500

    Update client.c

--- client.c
+++ client.c
@@ -281,6 +281,7 @@ void game_finish(struct game *game)
 
 	doneround = 1;
 
+	value = -1;
 	game->state = STATE_IDLE;
 	game->bet = 0;
 }
@@ -354,6 +355,7 @@ void cmd_stand(struct game *game, int argc, char *argv[])
 	while (hand_value(&game->dealer) < 17)
 		deck_deal(&game->deck, &game->dealer);
 
+	value = 0;
 	game_finish(game);
 }
 
@@ -510,6 +512,16 @@ int main(void)
 	while (count < 3 )
 	{
 		if(value == -1){
+			//check value
+			argv[0] = "HAND";
+			command(&game, 1, argv);
+			// printf("%d\n", value);
+		}
+		if(value == 0){
+			//BET money
+				argv[0] = "BET";
+				argv[1] = "1";
+				command(&game, 2, argv);
 			//check value
 			argv[0] = "HAND";
 			command(&game, 1, argv);
