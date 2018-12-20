92c2dc2731a98e92a3cb5afb3703df8836eb4462
Nandar Soe <38772142+snandar@users.noreply.github.com>
2018-12-20 02:18:01 -0500

    Update client.c

--- client.c
+++ client.c
@@ -100,6 +100,75 @@ int readline(int fd, char *buf, size_t maxlen)
 
 //Copied from Blackjack
 
+time_t boot;
+
+#define MAXARGS 32
+
+int card_value(char c)
+{
+        switch (c)
+        {
+                case 'A':
+                        return 1;
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                        return c - '0';
+                case 'T':
+                case 'J':
+                case 'Q':
+                case 'K':
+                        return 10;
+        }
+
+        return 0;
+}
+
+int hand_value(struct hand *h)
+{
+        int i;
+        int aces = 0;
+        int sum = 0;
+
+        for (i = 0; i < h->ncards; i++)
+        {
+                if (h->cards[i] == 'A')
+                        aces++;
+                else
+                        sum += card_value(h->cards[i]);
+        }
+
+        if (aces == 0)
+                return sum;
+
+        /* Give 11 points for aces, unless it causes us to go bust, so treat those as 1 point */
+        while (aces && (sum + aces*11) > 21)
+        {
+                sum++;
+                aces--;
+        }
+
+        sum += 11 * aces;
+
+        return sum;
+}
+
+enum { STATE_IDLE, STATE_PLAYING };
+
+struct game
+{
+        struct deck deck;
+        struct hand player;
+        struct hand dealer;
+        int state;
+        char *user;
+        int bet;
+};
 
 
 //Main function
