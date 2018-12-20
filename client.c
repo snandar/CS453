51a7945ff2145a71b7ce0e9df79a9424474211f6
Nandar Soe <38772142+snandar@users.noreply.github.com>
2018-12-20 02:14:28 -0500

    Update client.c

--- client.c
+++ client.c
@@ -100,59 +100,59 @@ int readline(int fd, char *buf, size_t maxlen)
 
 //Copied from Blackjack
 
-// int card_value(char c)
-// {
-// 	switch (c)
-// 	{
-// 	case 'A':
-// 		return 1;
-// 	case '2':
-// 	case '3':
-// 	case '4':
-// 	case '5':
-// 	case '6':
-// 	case '7':
-// 	case '8':
-// 	case '9':
-// 		return c - '0';
-// 	case 'T':
-// 	case 'J':
-// 	case 'Q':
-// 	case 'K':
-// 		return 10;
-// 	}
+int card_value(char c)
+{
+	switch (c)
+	{
+	case 'A':
+		return 1;
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+		return c - '0';
+	case 'T':
+	case 'J':
+	case 'Q':
+	case 'K':
+		return 10;
+	}
 
-// 	return 0;
-// }
+	return 0;
+}
 
-// int hand_value(struct hand *h)
-// {
-// 	int i;
-// 	int aces = 0;
-// 	int sum = 0;
+int hand_value(struct hand *h)
+{
+	int i;
+	int aces = 0;
+	int sum = 0;
 
-// 	for (i = 0; i < h->ncards; i++)
-// 	{
-// 		if (h->cards[i] == 'A')
-// 			aces++;
-// 		else
-// 			sum += card_value(h->cards[i]);
-// 	}
+	for (i = 0; i < h->ncards; i++)
+	{
+		if (h->cards[i] == 'A')
+			aces++;
+		else
+			sum += card_value(h->cards[i]);
+	}
 
-// 	if (aces == 0)
-// 		return sum;
+	if (aces == 0)
+		return sum;
 
-// 	/* Give 11 points for aces, unless it causes us to go bust, so treat those as 1 point */
-// 	while (aces && (sum + aces * 11) > 21)
-// 	{
-// 		sum++;
-// 		aces--;
-// 	}
+	/* Give 11 points for aces, unless it causes us to go bust, so treat those as 1 point */
+	while (aces && (sum + aces * 11) > 21)
+	{
+		sum++;
+		aces--;
+	}
 
-// 	sum += 11 * aces;
+	sum += 11 * aces;
 
-// 	return sum;
-// }
+	return sum;
+}
 
 enum
 {
@@ -201,13 +201,13 @@ void cmd_bet(struct game *game, int argc, char *argv[])
 {
         if (game->state == STATE_PLAYING)
         {
-                nprintf("-ERR You are already playing a game\n");
+                printf("-ERR You are already playing a game\n");
                 return;
         }
 
         if (strspn(argv[1], "1234567890") != strlen(argv[1]))
         {
-                nprintf("-ERR Expecting integer for a bet\n");
+                printf("-ERR Expecting integer for a bet\n");
                 return;
         }
 
